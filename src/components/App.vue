7d58a03d40f2c890e7f3a00844913fbcbde5f564
YA21 <ytik2013k@gmail.com>
2019-04-19 23:41:58 +0900

    refactor componets

---
+++ src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        hoge
+        <line-chart/>
+    </div>
+</template>
+
+<script>
+import { LineChart } from './LineChart'
+
+export default {
+    components: { LineChart }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,9 +1,10 @@
 <script>
-import { Line } from 'vue-chartjs'
+import Line from 'vue-chartjs'
 
 export default {
+    name: 'LineChart',
     extends: Line,
-    data () {
+    data: function() {
         return {
             data: {
                 labels: ['1', '2', '3', '4'],
@@ -19,7 +20,7 @@ export default {
             }
         }
     },
-    mounted () {
+    mounted: function() {
         console.log("hoge")
         this.renderChart(this.data, this.options)
     }
